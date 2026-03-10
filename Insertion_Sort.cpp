e7128dbb8daee0eac30e705cab1012570ebf274f
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-03-10 12:37:47 +0700

    input

---
+++ Insertion_Sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "=============================================";
+    cout << "==========Masukan Elemen Array==============";
+    cout << "=============================================";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
